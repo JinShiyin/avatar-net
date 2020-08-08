897b10f9b0feb709463f12e207e8eea8c96c391a
JinShiyin <shiyinjin@foxmail.com>
2020-08-08 10:05:19 +0800

    add .gitignore

--- evaluate_style_transfer.py
+++ evaluate_style_transfer.py
@@ -14,6 +14,7 @@ from models import preprocessing
 from PIL import Image
 
 slim = tf.contrib.slim
+# tf.reset_default_graph()
 
 
 tf.app.flags.DEFINE_string(
@@ -32,7 +33,7 @@ tf.app.flags.DEFINE_string(
 
 # choose the model configuration file
 tf.app.flags.DEFINE_string(
-    'model_config_path', None,
+    'model_config_path', 'configs/AvatarNet_config.yml',
     'The path of the model configuration file.')
 tf.app.flags.DEFINE_float(
     'inter_weight', 1.0,
@@ -88,6 +89,7 @@ def main(_):
     tf.logging.set_verbosity(tf.logging.INFO)
     with tf.Graph().as_default():
         # define the model
+        print(FLAGS.model_config_path)
         style_model, options = models_factory.get_model(FLAGS.model_config_path)
 
         # predict the stylized image
@@ -117,8 +119,10 @@ def main(_):
         content_image_filenames = get_image_filenames(FLAGS.content_dataset_dir)
 
         # starting inference of the images
-        init_fn = slim.assign_from_checkpoint_fn(
-          checkpoint_dir, slim.get_model_variables(), ignore_missing_vars=True)
+        # init_fn = slim.assign_from_checkpoint_fn(
+        #   checkpoint_dir, slim.get_model_variables(), ignore_missing_vars=True)
+        init_fn = slim.assign_from_checkpoint_fn("/data/jsy/code/avatar-net/pretrained_models/model.ckpt-120000",
+                                                 slim.get_model_variables(), ignore_missing_vars=True)
         with tf.Session() as sess:
             # initialize the graph
             init_fn(sess)
@@ -130,10 +134,10 @@ def main(_):
                 # gather the storage folder for the style transfer
                 style_label = style_image_filenames[i].split('/')[-1]
                 style_label = style_label.split('.')[0]
-                style_dir = os.path.join(FLAGS.eval_dir, style_label)
+                # style_dir = os.path.join(FLAGS.eval_dir, style_label)
 
-                if not tf.gfile.Exists(style_dir):
-                    tf.gfile.MakeDirs(style_dir)
+                if not tf.gfile.Exists(FLAGS.eval_dir):
+                    tf.gfile.MakeDirs(FLAGS.eval_dir)
 
                 # get the style image
                 np_style_image = image_reader(style_image_filenames[i])
@@ -142,6 +146,9 @@ def main(_):
                 for j in range(len(content_image_filenames)):
                     # gather the content image
                     np_content_image = image_reader(content_image_filenames[j])
+
+                    content_label = content_image_filenames[j].split('/')[-1]
+                    content_label = content_label.split('.')[0]
 
                     start_time = time.time()
                     np_stylized_image = sess.run(stylized_image,
@@ -152,8 +159,7 @@ def main(_):
                     total_time += incre_time
                     print("---%s seconds ---" % (total_time/nn))
 
-                    output_filename = os.path.join(
-                        style_dir, content_image_filenames[j].split('/')[-1])
+                    output_filename = os.path.join(FLAGS.eval_dir, f'{content_label}_{style_label}.bmp')
                     imsave(output_filename, np_stylized_image)
                     print('Style [%s]: Finish transfer the image [%s]' % (
                         style_label, content_image_filenames[j]))
